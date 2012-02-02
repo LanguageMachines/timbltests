15ba2a1bbf00e0b9ecda3baa9719b8bbb0463210
sloot <sloot@12f355fe-0486-481a-ad91-c297ab22b4e3>
2012-02-02 13:25:15 +0000

    oesp. 2012 has begun
    git-svn-id: https://ilk.uvt.nl/svn/tests/timbltests/trunk@14174 12f355fe-0486-481a-ad91-c297ab22b4e3

--- mydiff.sh
+++ mydiff.sh
@@ -12,7 +12,7 @@ if test -e $1
 then
   if test -e $2
   then
-      diff -w $1 $2 | grep -v "201[01]" |grep -v "\-\-\-" | grep -v "[0-9]\+[ac][0-9]\+" | grep -v "took [0-9]* seconds" | grep -v "Seconds taken:" | grep -v "based on" |grep -v $lh > md.tmp 2>& 1
+      diff -w $1 $2 | grep -v "201[012]" |grep -v "\-\-\-" | grep -v "[0-9]\+[ac][0-9]\+" | grep -v "took [0-9]* seconds" | grep -v "Seconds taken:" | grep -v "based on" |grep -v $lh > md.tmp 2>& 1
       exit `wc -c < md.tmp`
   else
       echo
