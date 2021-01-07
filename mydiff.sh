42266058a10d67a7270da78fce7a5e8930104981
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2021-01-07 12:52:15 +0100

    updated mydiff.sh

--- mydiff.sh
+++ mydiff.sh
@@ -6,7 +6,7 @@ if test -e $1
 then
   if test -e $2
   then
-      diff -w -B --ignore-matching-lines=".?Connection closed.*" $1 $2 | grep -v "[ -]20[12][09]" | grep -v "0K" |grep -v "\-\-\-" | grep -v "[0-9]\+[ac][0-9]\+" | grep -v "took [0-9]* seconds" | grep -v "Seconds taken:" | grep -v "based on" |grep -v $lh |grep -v "Client on node:" > md.tmp 2>& 1
+      diff -w -B --ignore-matching-lines=".?Connection closed.*" $1 $2 | grep -v "[ -]20[12][0-9]" | grep -v "0K" |grep -v "\-\-\-" | grep -v "[0-9]\+[ac][0-9]\+" | grep -v "took [0-9]* seconds" | grep -v "Seconds taken:" | grep -v "based on" |grep -v $lh |grep -v "Client on node:" > md.tmp 2>& 1
       exit `wc -c < md.tmp`
   else
       echo
